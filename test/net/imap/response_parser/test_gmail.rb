a7d649773f050c524243ece2ee4fe3c108e34848
Will Robertson <willjr@google.com>
2013-01-03 21:22:00 +0000

    Add a test case

--- test/net/imap/response_parser/test_gmail.rb
+++ test/net/imap/response_parser/test_gmail.rb
@@ -12,4 +12,13 @@ class Net::IMAP::ResponseParser::GmailTest < Test::Unit::TestCase
     assert_equal 789, result.data.attr['X-GM-THRID']
     assert_equal ['Foo', 'Bar'], result.data.attr['X-GM-LABELS']
   end
+
+  def test_parses_gmail_fetch_quoted_labels
+    message = File.read(File.expand_path(File.join(__FILE__, '..', '..', '..', '..', 'fixtures', 'response-quoted-labels.log')))
+
+    parser = Net::IMAP::ResponseParser::Gmail.new
+    result = parser.parse(message)
+
+    assert_equal ['Inbox', 'This', 'Supports Spaces'], result.data.attr['X-GM-LABELS']
+  end
 end
